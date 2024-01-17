a22d75c49e912e133e902f31bff0793ac2d778c7
Matteo_Bonino <matteobonino@hotmail.com>
2024-01-17 15:23:32 +0100

    Created table, populated it

--- main.py
+++ main.py
@@ -3,8 +3,19 @@ import mysql.connector
 mydb = mysql.connector.connect(
     host="DESKTOP-OTMM446",
     user="admin",
-    passwd="admin"
-
+    passwd="admin",
+    database="testdb"
 )
 
-print(mydb)
+mycursor = mydb.cursor()
+
+sqlFormula = "INSERT INTO students (name, age) VALUES (%s, %s)"
+students = [("Avi", 22),
+            ("Bob", 22),
+            ("Amanda", 22),
+            ("Jacob", 22),
+            ("Michelle", 22)]
+
+mycursor.executemany(sqlFormula, students)
+
+mydb.commit()
